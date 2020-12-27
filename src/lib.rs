adc8645689b468563a6fce11f334b5f1ff06485d
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-12-27 04:25:09 -0300

    to_decimal: bug fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub fn to_binary(number: &f64) -> f64 {
+pub fn to_binary(number: &f64) -> String {
     
     let mut number_as_integer: u128 = number.trunc().to_string().parse().unwrap();
     let mut bits_string = String::from("");
@@ -35,41 +35,37 @@ pub fn to_binary(number: &f64) -> f64 {
         }
     }
     //----------------------------------
-    return bits_string.parse::<f64>().unwrap();
+    bits_string
 }
 
 pub fn to_decimal(number: &f64) -> f64 {
 
     let bits_string: &str = &number.to_string();
-    let reverse_bits_string: &str = &bits_string.chars().rev().collect::<String>();
     let bits_string_iterator = bits_string.split("");
-    let reverse_bits_string_iterator = reverse_bits_string.split("");
     let mut decimal_number_result: f64 = 0.0;
+    let mut is_fractional_part = false;
+    let mut denominator: f64 = 2.0;
 
-    //The part aboves convert the integer part of the number to binary
+    //The part aboves convert the number to binary
     for i in bits_string_iterator {
         match i {
-            "1" => decimal_number_result = decimal_number_result * 2.0 + 1.0,
-            "0" => decimal_number_result = decimal_number_result * 2.0,
-            "." => break,
-            _ => ()
-        }
-    }
-    //----------------------------------
-
-    //The aboves convert the fractional part of the number to decimal if it is bigger than 0.0
-    if number.fract() != 0.0 {
-        let mut denominator: f64 = 2.0;
-        for j in reverse_bits_string_iterator {
-            match j {
-                "1" => {
+            "1" => {
+                if is_fractional_part == false {
+                    decimal_number_result = decimal_number_result * 2.0 + 1.0
+                } else {
                     decimal_number_result += 1.0/denominator;
                     denominator *= 2.0;
-                },
-                "0" => denominator *= 2.0,
-                "." => break,
-                _ => ()
-            }
+                }
+            },
+            "0" => {
+                if is_fractional_part == false {
+                    decimal_number_result = decimal_number_result * 2.0;
+                } else {
+                    denominator *= 2.0;
+                }
+            },
+            "." => is_fractional_part = true,
+            _ => ()
         }
     }
     //----------------------------------
